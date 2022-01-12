bff7bad3e7b17b99f4264b3a350eb8e37dbcb72f
Alejandro <aariasmartijena@gmail.com>
2022-01-12 11:38:59 -0400

    change folder names

--- part01/1.6-1.11/src/App.js
+++ part01/1.6-1.11/src/App.js
@@ -34,8 +34,8 @@ const Statistics = (props) => {
           <StatisticLine text="good" value={props.good} />
           <StatisticLine text="neutral" value={props.neutral} />
           <StatisticLine text="bad" value={props.bad} />
-          <StatisticLine text="average" value={(props.good - props.bad) / props.total} />
-          <StatisticLine text="positive" value={(props.good / props.total) * 100} />
+          <StatisticLine text="average" value={((props.good - props.bad) / props.total).toFixed(2)} />
+          <StatisticLine text="positive" value={((props.good / props.total) * 100).toFixed(2)} />
         </tbody>
       </table>
     )
