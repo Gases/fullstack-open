086bb12b1675243e08084165a0306cedd02584a5
Alejandro <aariasmartijena@gmail.com>
2022-01-12 10:40:50 -0400

    solve exercises 1.7 and 1.8

--- part01/1.6-1.14/src/App.js
+++ part01/1.6-1.14/src/App.js
@@ -5,6 +5,20 @@ const Button = (props) => {
     <button onClick={props.handleClick}>
       {props.text}
     </button>
+  );
+};
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.total}</p>
+      <p>average {(props.good + props.bad) / props.total}</p>
+      <p>positive {(props.good / props.total) * 100}%</p>
+    </div>
   )
 }
 
@@ -12,10 +26,22 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  const handleGood = () => setGood(good + 1);
-  const handleNeutral = () => setNeutral(neutral + 1);
-  const handleBad = () => setBad(bad + 1);
+  const handleGood = () => {
+    setGood(good + 1);
+    setTotal(total + 1);
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setTotal(total + 1);
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1);
+    setTotal(total + 1);
+  }
 
   return (
     <div>
@@ -25,10 +51,7 @@ const App = () => {
       <Button text="neutral" handleClick={handleNeutral} />
       <Button text="bad" handleClick={handleBad} />
 
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
     </div>
   );
 };
