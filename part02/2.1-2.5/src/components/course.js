bd07b1dd2d28e78702c1985aa88ccd28eea88708
Alejandro <aariasmartijena@gmail.com>
2022-01-16 18:28:51 -0400

    solved exercises 2.4 and 2.5

---
+++ part02/2.1-2.5/src/components/course.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const Parts = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
+
+const Header = (props) => {
+  return (
+      <h2>{props.course}</h2>
+  );
+};
+
+const Content = ({ parts }) => {
+  
+  return (
+    <div>
+      {parts.map(part => 
+        <Parts key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const exercises = parts.map(part => part.exercises);
+
+  return (
+    <p>
+      <b>Total number of exercises: {exercises.reduce((a,b) => a + b)}</b>
+    </p>
+  );
+};
+
+const Course = ({ courses }) => {
+  return (
+    <div>
+      {courses.map(course => 
+        <div>
+          <h1>Web development curriculum</h1>
+          <Header course={course.name}/>
+          <Content parts={course.parts}/>
+          <Total parts={course.parts} />
+        </div>
+      )}
+    </div>
+  )
+};
+
+export default Course;
