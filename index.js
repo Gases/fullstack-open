1dcad70b5119c5093f2df45f073037c5002b48a5
Gases <aariasmartijena@gmail.com>
2022-03-11 17:50:25 -0400

    uibuild

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+let persons = [
+  { 
+    id: 1,
+    name: "Arto Hellas", 
+    number: "040-123456"
+  },
+  { 
+    id: 2,
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
+  },
+  { 
+    id: 3,
+    name: "Dan Abramov", 
+    number: "12-43-234345"
+  },
+  { 
+    id: 4,
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
+  }
+];
+
 app.use(express.json());
 app.use(cors());
 app.use(express.static('build'));
@@ -17,29 +40,6 @@ morgan.token('body', (req, res) => {
 });
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-];
 
 app.get('/api/persons', (req, res) => {
   res.json(persons);
@@ -94,7 +94,7 @@ app.post('/api/persons', (req, res) => {
 
   persons = persons.concat(person);
 
-  res.json(persons);
+  res.json(person);
 });
 
 app.listen(PORT, () => {
